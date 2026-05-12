d232ad59e928fe4c9e96659438534ece56df6d16
SheillaO <sheilla.alusa@strathmore.edu>
2026-05-12 13:57:49 +0300

    Added nodedemon

--- utils/sightingEvents.js
+++ utils/sightingEvents.js
@@ -1,4 +1,7 @@
 import {eventEmitter} from 'node:events'
 import { createAlert } from './createAlert'
+import { EventEmitter } from 'node:stream'
 
-export const sightingEvents = new E
+export const sightingEvents = new EventEmitter()
+
+sightingEvents.on('sighting-added', createAlert)
