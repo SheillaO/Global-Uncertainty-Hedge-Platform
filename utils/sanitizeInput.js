aa79e86c506769a5b982442a706b6a4219c338bb
SheillaO <sheilla.alusa@strathmore.edu>
2026-05-12 13:14:47 +0300

    Update sanitizeInput.js

--- utils/sanitizeInput.js
+++ utils/sanitizeInput.js
@@ -1,1 +1,17 @@
-import 
+import sanitizehtml from 'sanitizehtml'
+
+export function sanitizeInput (data){
+    const sanitizedData = {}
+
+    for (const [key,value]) of Object.entries(data)){
+
+        if (typeof value ==='string') {
+            sanitizedData[key] = sanitizehtml (value, {
+             allowedTags:[], allowed
+
+            })
+        }
+    }
+
+
+}
